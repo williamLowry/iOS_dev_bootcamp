967845ec1dd2fae8cf28cc1e3f4162f967b6bb64
Will Lowry <will_lowry@yahoo.com>
2019-06-08 17:10:11 -0500

    Complete Dicee application

--- project03/Dicee/Dicee/ViewController.swift
+++ project03/Dicee/Dicee/ViewController.swift
@@ -28,6 +28,10 @@ class ViewController: UIViewController {
         updateDiceImages()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
     func updateDiceImages() {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
